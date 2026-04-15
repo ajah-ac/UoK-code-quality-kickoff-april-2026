c3220a4c9f3d243940650bb8411dccd32a7c615c
ajah <ajahajang00@gmail.com>
2026-04-15 16:11:03 +0200

    Validate Password

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -20,23 +20,40 @@ export default function isValidPassword(password = "") {
    *   return ...;
    * }
    */
-  const regex=/[a-zA-z0-9]/gi
+const regex = /^[a-z0-9]+$/i
 const tested=regex.test(password)
 
-if(password.length!==10){
-  return false
-}
 
-if(tested!==true){
-  return false
+const ascedence=(pass)=>{
+pass=pass.replace(/[^0-9]/g,'')
+  let ascend=0;
+  let descend=0
+  for(let i=0;i<pass.length-1;i++){
+    if(Number(pass[i])-1===Number(pass[i+1])){
+      ascend++
+      descend=0
+    }
+    else if(Number(pass[i])+1===Number(pass[i+1])){
+      descend++
+    ascend=0}
+      else {
+        descend=0
+      ascend=0}
+        if(ascend>=3 || descend>=3) return false
 
+  }
+    
+    return true
 }
+if(!ascedence(password)) return false
+if(password.length!==10)return false
+if(tested!==true) return false 
 if(forbiddenPasswords.includes(password)) return false
-
-if(password.split('').every(e=>e===e.toLowerCase() || e===e.toUpperCase())) return false
-
-  const setOfPassword = new Set(password.split(''));
+if(password.split('').every(char=>/[a-z]/gi.test(char))) return false
+if (password === password.toLowerCase() || password === password.toUpperCase()) return false;  
+const setOfPassword = new Set(password.split(''));
   if (setOfPassword.size < 4) return false;
   return true;
 }
+
 
